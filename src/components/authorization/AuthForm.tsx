009778febd34ae15c3d7c850c73a51811ebe2841
OleksiyBubenko <ipz202_bov@student.ztu.edu.ua>
2023-07-03 15:04:11 +0300

    Added formik

--- src/components/authorization/AuthForm.tsx
+++ src/components/authorization/AuthForm.tsx
@@ -1,79 +1,82 @@
-import {Button, Box, TextField} from "@mui/material";
-import React, {useState} from "react";
+import React from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { Box, TextField, Button } from '@mui/material';
 
 interface AuthFormData {
-    login: string,
-    password: string
+    login: string;
+    password: string;
 }
+
+const validationSchema = Yup.object().shape({
+  login: Yup.string().email('Incorrect login').required('Login is required'),
+  password: Yup.string().required('Password is required').min(6, 'Password must be at least 6 characters'),
+});
+
 const initialAuthData: AuthFormData = {
-  login: "",
-  password: ""
+    login: '',
+    password: '',
 };
+
 export default function AuthForm() {
-    const [authData,
-        setAuthData] = useState(initialAuthData);
+    const handleSubmit = (values: AuthFormData, {resetForm}: any) => {
+        // Call action epic to send data to server
+        console.log(values);
+        resetForm();
+    };
 
-    function handleLoginChange(e: React.ChangeEvent<HTMLInputElement>) {
-        setAuthData({
-            ...authData,
-            login: e.target.value
-        });
-    }
-    function handlePasswordChange(e: React.ChangeEvent<HTMLInputElement>) {
-        setAuthData({
-            ...authData,
-            password: e.target.value
-        });
-    }
-
-    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-        e.preventDefault();
-        //call action epic to send data to server
-        console.log(authData);
-        setAuthData(initialAuthData);
-    }
+    const formik = useFormik({
+        initialValues: initialAuthData,
+        validationSchema,
+        onSubmit: handleSubmit,
+    });
 
     return (
-        <div>
-            <Box sx={
-                {
-                    color: 'secondary.main',
-                    fontFamily: 'default',
-                    fontSize: 20,
-                    mb: 3
-                }
-            }>You need to sign in first</Box>
-            <form onSubmit={handleSubmit}>
-                <TextField
-                    type="text"
-                    variant='outlined'
-                    color='secondary'
-                    label="Login"
-                    onChange={handleLoginChange}
-                    value={authData.login}
-                    fullWidth
-                    required
-                    sx={{mb: 4}}
-                />
-                <TextField
-                    type="password"
-                    variant='outlined'
-                    color='secondary'
-                    label="Password"
-                    onChange={handlePasswordChange}
-                    value={authData.password}
-                    required
-                    fullWidth
-                    sx={{mb: 4}}
-                />
-                <Button
-                    variant="outlined"
-                    color="secondary"
-                    type="submit"
-                >
-                    Sign in
-                </Button>
-            </form>
-        </div>
+      <div>
+          <Box
+            sx={{
+                color: 'secondary.main',
+                fontFamily: 'default',
+                fontSize: 20,
+                mb: 3,
+            }}
+          >
+              You need to sign in first
+          </Box>
+          <form onSubmit={formik.handleSubmit}>
+              <TextField
+                type="text"
+                variant="outlined"
+                color="secondary"
+                label="Login"
+                {...formik.getFieldProps('login')}
+                fullWidth
+                required
+                sx={{ mb: 4 }}
+                error={formik.touched.login && !!formik.errors.login}
+                helperText={formik.touched.login && formik.errors.login}
+              />
+              <TextField
+                type="password"
+                variant="outlined"
+                color="secondary"
+                label="Password"
+                {...formik.getFieldProps('password')}
+                required
+                fullWidth
+                sx={{ mb: 4 }}
+                error={formik.touched.password && !!formik.errors.password}
+                helperText={formik.touched.password && formik.errors.password}
+              />
+              <Button
+                variant="outlined"
+                color="secondary"
+                type="submit"
+                disabled={formik.isSubmitting}
+              >
+                  Sign in
+              </Button>
+          </form>
+      </div>
     );
 }
