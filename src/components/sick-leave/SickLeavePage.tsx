424517253a5c245afd82696930a0564f6e98caea
Davyd Melnychenko <ipz204_mdv@student.ztu.edu.ua>
2023-08-19 16:02:38 +0300

    sick leave filter component

--- src/components/sick-leave/SickLeavePage.tsx
+++ src/components/sick-leave/SickLeavePage.tsx
@@ -1,27 +1,32 @@
 import {useAppDispatch, useAppSelector} from "../../redux/CustomHooks";
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {getSickLeaveDataActionCreator} from "../../redux/epics/SickLeaveEpics";
-import moment from "moment";
+import moment, {Moment} from "moment";
 import {
     Alert,
 } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import {Outlet} from "react-router-dom";
 import SickLeaveList from "./SickLeaveList";
+import SickLeaveListFilters from "./SickLeaveListFilters";
+import Profile from "../../models/Profile";
 
 export default function SickLeavePage() {
     const dispatch = useAppDispatch();
 
     const {error, isLoading} = useAppSelector(state => state.sickLeave);
-    const {user} = useAppSelector(state => state.user);
+
+    const [filterUser, setFilterUser] = useState<Profile | null>(null);
+    const [searchByYear, setSearchByYear] = useState<boolean>(false);
+    const [filterDate, setFilterDate] = useState<Moment>(moment());
 
     useEffect(() => {
         dispatch(getSickLeaveDataActionCreator({
-            userId: user.id,
-            date: moment(),
-            searchByYear: false
+            userId: filterUser ? filterUser.id : null,
+            date: filterDate,
+            searchByYear: searchByYear
         }))
-    }, []);
+    }, [filterUser, searchByYear, filterDate]);
 
     return (
         <>
@@ -38,6 +43,14 @@ export default function SickLeavePage() {
 
                             <Outlet />
 
+                            <SickLeaveListFilters
+                                filterUser={filterUser}
+                                setFilterUser={setFilterUser}
+                                searchByYear={searchByYear}
+                                setSearchByYear={setSearchByYear}
+                                filterDate={filterDate}
+                                setFilterDate={setFilterDate}
+                            />
                             <SickLeaveList />
                         </>
                     }
